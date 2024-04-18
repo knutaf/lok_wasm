65480e31ae2fc6d4f519f4c7a3e229210fcb04a8
knutaf <knutaf@gmail.com>
2024-04-18 00:12:37 -0700

    Passes simple array of characters to JS

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,33 @@
 mod utils;
+mod grid;
+
+use crate::grid::Grid;
+
+#[wasm_bindgen]
+pub struct Board {
+    grid: Grid<u8>,
+}
+
+#[wasm_bindgen]
+impl Board {
+    pub fn new() -> Board {
+        Board {
+            grid: Grid::new(6, 4, &('!' as u8)),
+        }
+    }
+
+    pub fn width(&self) -> u32 {
+        self.grid.width() as u32
+    }
+
+    pub fn height(&self) -> u32 {
+        self.grid.height() as u32
+    }
+
+    pub fn cells(&self) -> *const u8 {
+        self.grid.cells()
+    }
+}
 
 use wasm_bindgen::prelude::*;
 
